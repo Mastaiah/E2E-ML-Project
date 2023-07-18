4ac35767657c3872411ae2d0911fcbec4d9147b7
Mastaiah <niranjan.mastaiah@gmail.com>
2023-07-18 19:12:02 +0100

    Updated exception code

--- src/utils/exception.py
+++ src/utils/exception.py
@@ -1,12 +1,5 @@
 import sys
 import os
-import inspect
-from utils.logger import CustomLogger
-
-"""def error_message_detail(message , filename , lineno):
-    error_message = f"The exception has occured in python file {filename} line number {lineno} message {str(message)}"   
-    return error_message"""
-
 
 #Custom exception class
 class CustomException(Exception):
